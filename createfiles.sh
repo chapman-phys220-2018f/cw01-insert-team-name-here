2dbd6c786ad291f66d758a06652c2c1c21a73b50
Justin Dressel <dressel@chapman.edu>
2018-09-10 23:48:43 -0700

    INSTRUCTOR COMMENT

--- createfiles.sh
+++ createfiles.sh
@@ -8,6 +8,13 @@ mkdir ~/PHYS220/info/tmpfiles
 
 cd ~/PHYS220/info/tmpfiles
 
+# INSTRUCTOR COMMENT
+# Better to use relative paths above. You have absolute paths,
+# which may not be the same on different computers. At least with
+# relative paths, they are consistent with respect to where the 
+# git directory is cloned.
+
+
 # create 100 files in directory
 
 for i in $(seq 100); do echo > file${i}.tmp; done
